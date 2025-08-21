e0aa68066f96f2d198c6877833d4e5f89ea0713f
Martin Ulstein <6736558+mjulstein@users.noreply.github.com>
2025-08-21 20:38:14 +0200

    react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { App } from './App';
 import { RouterProvider } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,9 @@ createRoot(document.getElementById('root')!).render(
       <RouterProvider>
         <App />
       </RouterProvider>
+      {import.meta.env.DEV && (
+        <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      )}
     </QueryClientProvider>
   </StrictMode>,
 );
